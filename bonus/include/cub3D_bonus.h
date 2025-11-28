ba83d28ccac40acea87482c0883f06ff36d82927
Zakaria Talbi <zakariatalbiofficial@gmail.com>
2025-11-28 06:13:16 +0100

    some cleanup

--- bonus/include/cub3D_bonus.h
+++ bonus/include/cub3D_bonus.h
@@ -6,7 +6,7 @@
 /*   By: zatalbi <zatalbi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 19:33:02 by zatalbi           #+#    #+#             */
-/*   Updated: 2025/11/27 13:50:01 by zatalbi          ###   ########.fr       */
+/*   Updated: 2025/11/28 03:01:50 by zatalbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,64 +20,88 @@
 # include <string.h>
 # include <sys/time.h>
 # include <math.h>
-#include <ctype.h>
-#include  <limits.h>
+# include <ctype.h>
+# include <limits.h>
 # include "MLX42.h"
 
-/* *** minimap *** */
-# define P_EYE 0xFFB4DCFF
-# define P_PODY 0xC8325AFF
-# define M_OUT 0x141419FF
-# define M_WALL 0x2D2D37FF
-# define M_FLOOR 0x649B64FF
-# define M_C_DOOR 0x3B1F0FFF
-# define M_O_DOOR 0x5A2F17FF
+/* *** minimap config *** */
+# ifndef P_EYE
+#  define P_EYE 0xFFB4DCFF
+# endif
 
-/* *** animation *** */
-# define FRAMES_DIR "./bonus/frames"
+# ifndef P_PODY
+#  define P_PODY 0xC8325AFF
+# endif
 
-/* *** door *** */
-# define DOOR_TEX_PATH "./textures/door.png"
+# ifndef M_OUT
+#  define M_OUT 0x141419FF
+# endif
 
-# define WIDTH 1280
-# define HEIGHT 720
+# ifndef M_WALL
+#  define M_WALL 0x2D2D37FF
+# endif
 
+# ifndef M_FLOOR
+#  define M_FLOOR 0x649B64FF
+# endif
+
+# ifndef M_C_DOOR
+#  define M_C_DOOR 0x3B1F0FFF
+# endif
+
+# ifndef M_O_DOOR
+#  define M_O_DOOR 0x5A2F17FF
+# endif
+
+/* *** animation config *** */
+# ifndef FRAMES_DIR
+#  define FRAMES_DIR "./bonus/frames"
+# endif
+
+/* *** door config *** */
+# ifndef DOOR_TEX_PATH
+#  define DOOR_TEX_PATH "./textures/door.png"
+# endif
+
+/* *** window size *** */
+# ifndef WIDTH
+#  define WIDTH 1280
+# endif
+
+# ifndef HEIGHT
+#  define HEIGHT 720
+# endif
+
+/* ******************************* raycasting ******************************* */
+
+/* *** textures id *** */
 # define NO 0
 # define SO 1
 # define WE 2
 # define EA 3
 # define DR 4
 
+/* *** pi value *** */
 # define PI 3.141593
 
 typedef unsigned int	t_u_int;
 typedef long long		t_time;
 
+/* *** int point *** */
 typedef struct s_i_xy
 {
 	int	x;
 	int	y;
 }	t_i_xy;
 
+/* *** double point *** */
 typedef struct s_d_xy
 {
 	double	x;
 	double	y;
 }	t_d_xy;
 
-typedef struct s_dist
-{
-	t_d_xy	side;
-	t_d_xy	delta;
-}	t_dist;
-
-typedef struct s_tex
-{
-	int	x;
-	int	y;
-	int	id;
-}	t_tex;
-
+/* *** player struct *** */
 typedef struct s_player
 {
 	t_d_xy	pos;
@@ -85,6 +109,22 @@ typedef struct s_player
 	t_d_xy	plane;
 }	t_player;
 
+/* *** tex struct *** */
+typedef struct s_tex
+{
+	int	x;
+	int	y;
+	int	id;
+}	t_tex;
+
+/* *** dist *** */
+typedef struct s_dist
+{
+	t_d_xy	side;
+	t_d_xy	delta;
+}	t_dist;
+
+/* *** line *** */
 typedef struct s_line
 {
 	int	height;
@@ -92,6 +132,7 @@ typedef struct s_line
 	int	end;
 }	t_line;
 
+/* *** ray *** */
 typedef struct s_ray
 {
 	t_d_xy	dir;
@@ -102,16 +143,21 @@ typedef struct s_ray
 	int		isdoor;
 }	t_ray;
 
-typedef struct	s_door
+/* *** door struct *** */
+typedef struct s_door
 {
 	t_i_xy	pos;
 	t_time	open_t;
 	int		isopen;
 }	t_door;
 
-#define FRAMES 42
+/* *** animation frames *** */
+# ifndef FRAMES
+#  define FRAMES 42
+# endif
 
-typedef struct	s_anime
+/* *** animation struct *** */
+typedef struct s_anime
 {
 	mlx_image_t		*img;
 	mlx_texture_t	*frames[FRAMES];
@@ -120,6 +166,7 @@ typedef struct	s_anime
 	int				enabled;
 }	t_anime;
 
+/* *** data struct *** */
 typedef struct s_data
 {
 	mlx_t			*mlx;
@@ -129,7 +176,7 @@ typedef struct s_data
 	char			*tex_path[4];
 	t_u_int			ceil;
 	t_u_int			floor;
-    char			**map;
+	char			**map;
 	t_i_xy			map_size;
 	t_player		player;
 	int				mouse;
@@ -137,42 +184,54 @@ typedef struct s_data
 	t_door			door[2];
 }	t_data;
 
-int		ft_cub_init(t_data *data, int argc, char **argv);
-int		ft_cub_loop(t_data *data);
+/* *** game init *** */
+int			ft_cub_init(t_data *data, int argc, char **argv);
 
-void	ft_cub(void *param);
-void	ft_draw(t_data *data, t_ray *ray, t_i_xy *xy);
+/* *** game loop *** */
+int			ft_cub_loop(t_data *data);
 
-void	ft_mini_map(void *param);
+/* *** raycasting func *** */
+void		ft_cub(void *param);
 
-void	ft_move_player(void *param);
-void	ft_rotate1(t_data *data, double rs);
-void	ft_rotate2(t_data *data, double rs);
+/* *** rendering func *** */
+void		ft_draw(t_data *data, t_ray *ray, t_i_xy *xy);
 
-void	ft_mouse_rotate(double xpos, double ypos, void *param);
-void	ft_mouse_mode(mouse_key_t button, action_t action, modifier_key_t mods,
-			void *param);
+/* *** mini map *** */
+void		ft_mini_map(void *param);
 
-int		ft_iswall(t_data *data, double x, double y);
-t_u_int	ft_color(t_u_int r, t_u_int g, t_u_int b, t_u_int a);
+/* *** moving funcs *** */
+void		ft_move_player(void *param);
+void		ft_rotate1(t_data *data, double rs);
+void		ft_rotate2(t_data *data, double rs);
 
-int		ft_anime_init(t_data *data, t_anime *anime);
-void	ft_animation(void *param);
+/* *** mouse funcs *** */
+void		ft_mouse_rotate(double xpos, double ypos, void *param);
+void		ft_mouse_mode(mouse_key_t button, action_t action,
+				modifier_key_t mods, void *param);
 
-void	ft_door(t_data *data);
-void	ft_door_hook(void *param);
-	
-/* **************************** parcing ***************************** */
+/* *** door funcs *** */
+void		ft_door(t_data *data);
+void		ft_door_hook(void *param);
+
+/* *** animation *** */
+int			ft_anime_init(t_data *data, t_anime *anime);
+void		ft_animation(void *param);
+
+/* *** utils funcs *** */
+int			ft_iswall(t_data *data, double x, double y);
+t_u_int		ft_color(t_u_int r, t_u_int g, t_u_int b, t_u_int a);
+
+/* ********************************* parcing ******************************** */
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
 
-typedef struct	s_point
+typedef struct s_point
 {
-	int x;
-	int y;
-} t_point;
+	int	x;
+	int	y;
+}	t_point;
 
 typedef struct s_gc
 {
@@ -182,86 +241,84 @@ typedef struct s_gc
 
 typedef struct s_textures
 {
-	char *no;
-	char *so;
-	char *we;
-	char *ea;
-} t_textures;
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+}	t_textures;
 
 typedef struct s_color
 {
-	int r;
-	int g;
-	int b;
-} t_color;
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
 
 typedef struct s_config
 {
-	t_textures tex;
-	t_color floor;
-	t_color ceil;
-	char **map;
-	int player_x;
-	int player_y;
-	char player_dir;
-	int y_rows;
-	int x_cols;
-} t_config;
+	t_textures	tex;
+	t_color		floor;
+	t_color		ceil;
+	char		**map;
+	int			player_x;
+	int			player_y;
+	char		player_dir;
+	int			y_rows;
+	int			x_cols;
+}	t_config;
 
 //malloc_free;
-void	*gc_malloc(size_t size);
-void free_ptr(void *ptr);
-void	free_all(void);
-char *gc_strndup(const char *line, int start, int end);
-char	*ft_strdup(char *s1);
+void		*gc_malloc(size_t size);
+void		free_ptr(void *ptr);
+void		free_all(void);
+char		*gc_strndup(const char *line, int start, int end);
+char		*ft_strdup(char *s1);
 
 //parcing
-int main_parce(char **cub_file, t_config *data);
-void	data_init(t_config *conf);
-int parce_line(char **file, int fd,t_config *data);
-int assign_color(char *start,char *line, int i, t_config *data);
+int			main_parce(char **cub_file, t_config *data);
+void		data_init(t_config *conf);
+int			parce_line(char **file, int fd, t_config *data);
+int			assign_color(char *start, char *line, int i, t_config *data);
 // int assign_map(char *line, t_config *data);
-int is_map(char *line);
-int negative_map_position(t_config *data);
-int only_spaces(char *s);
-int get_cont_line(char *file);
-char *get_start(char *line, int i);
-char *get_color(char *line, int i);
+int			is_map(char *line);
+int			negative_map_position(t_config *data);
+int			only_spaces(char *s);
+int			get_cont_line(char *file);
+char		*get_start(char *line, int i);
+char		*get_color(char *line, int i);
 
 //utils
-int	ft_isdigit(int v);
-int	ft_atoi(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_strcmp(char *s1, char *s2);
-int is_space(char c);
-
+int			ft_isdigit(int v);
+int			ft_atoi(const char *str);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(char *s1, char *s2);
+int			is_space(char c);
 
 //validation
-int validation(t_config *data);
-int is_player(char c);
-int	flood_fill(char **map, t_point p, int height, int width);
-int validation_char_map(t_config * data);
-char **prepar_map(t_config *data);
-int get_long(t_config *data);
-int get_h(char**map);
-int	fill_inside(char **map, t_point p, t_config *data);
-int check_inside(char **map, t_config *data);
-void assing_position(int y, int x, t_config *data);
-int map_char(char c, t_config *data, int x, int y);
-int assign_tex(char *start,char *line, int i, t_config *data);
-int check(int fd);
-int parcing_dor(t_config *data, int y, int x);
-void	find_and_replace_player(char **map, t_config *data);
-
+int			validation(t_config *data);
+int			is_player(char c);
+int			flood_fill(char **map, t_point p, int height, int width);
+int			validation_char_map(t_config *data);
+char		**prepar_map(t_config *data);
+int			get_long(t_config *data);
+int			get_h(char**map);
+int			fill_inside(char **map, t_point p, t_config *data);
+int			check_inside(char **map, t_config *data);
+void		assing_position(int y, int x, t_config *data);
+int			map_char(char c, t_config *data, int x, int y);
+int			assign_tex(char *start, char *line, int i, t_config *data);
+int			check(int fd);
+int			parcing_dor(t_config *data, int y, int x);
+void		find_and_replace_player(char **map, t_config *data);
 
 //grtnext_line
-char	*get_next_line(int fd);
-char	*ft_read_to_left_str(int fd, char *left_str);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *s);
-char	*ft_strjoin(char *left_str, char *buff);
-char	*ft_get_line(char *left_str);
-char	*ft_new_left_str(char *left_str);
+char		*get_next_line(int fd);
+char		*ft_read_to_left_str(int fd, char *left_str);
+char		*ft_strchr(char *s, int c);
+size_t		ft_strlen(char *s);
+char		*ft_strjoin(char *left_str, char *buff);
+char		*ft_get_line(char *left_str);
+char		*ft_new_left_str(char *left_str);
 t_config	*parcing(int ac, char **av);
 
 #endif
--- include/cub3D.h
+++ include/cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaboudra <aaboudra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zatalbi <zatalbi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 19:33:02 by zatalbi           #+#    #+#             */
-/*   Updated: 2025/11/22 16:11:05 by aaboudra         ###   ########.fr       */
+/*   Updated: 2025/11/28 03:23:51 by zatalbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,47 +21,46 @@
 # include <sys/time.h>
 # include <math.h>
 # include <ctype.h>
-# include  <limits.h>
+# include <limits.h>
 # include "MLX42.h"
 
+/* *** window size *** */
+# ifndef WIDTH
+#  define WIDTH 1280
+# endif
 
-# define WIDTH 1280
-# define HEIGHT 720
+# ifndef HEIGHT
+#  define HEIGHT 720
+# endif
 
+/* ******************************* raycasting ******************************* */
+
+/* *** textures id *** */
 # define NO 0
 # define SO 1
 # define WE 2
 # define EA 3
 
+/* *** pi value *** */
 # define PI 3.141593
 
 typedef unsigned int	t_u_int;
 
+/* *** int point *** */
 typedef struct s_i_xy
 {
 	int	x;
 	int	y;
 }	t_i_xy;
 
+/* *** double point *** */
 typedef struct s_d_xy
 {
 	double	x;
 	double	y;
 }	t_d_xy;
 
-typedef struct s_dist
-{
-	t_d_xy	side;
-	t_d_xy	delta;
-}	t_dist;
-
-typedef struct s_tex
-{
-	int	x;
-	int	y;
-	int	id;
-}	t_tex;
-
+/* *** player struct *** */
 typedef struct s_player
 {
 	t_d_xy	pos;
@@ -69,6 +68,22 @@ typedef struct s_player
 	t_d_xy	plane;
 }	t_player;
 
+/* *** tex struct *** */
+typedef struct s_tex
+{
+	int	x;
+	int	y;
+	int	id;
+}	t_tex;
+
+/* *** dist *** */
+typedef struct s_dist
+{
+	t_d_xy	side;
+	t_d_xy	delta;
+}	t_dist;
+
+/* *** line *** */
 typedef struct s_line
 {
 	int	height;
@@ -76,6 +91,7 @@ typedef struct s_line
 	int	end;
 }	t_line;
 
+/* *** ray *** */
 typedef struct s_ray
 {
 	t_d_xy	dir;
@@ -85,6 +101,7 @@ typedef struct s_ray
 	int		side;
 }	t_ray;
 
+/* *** data struct *** */
 typedef struct s_data
 {
 	mlx_t			*mlx;
@@ -93,118 +110,124 @@ typedef struct s_data
 	char			*tex_path[4];
 	t_u_int			ceil;
 	t_u_int			floor;
-    char			**map;
+	char			**map;
 	t_i_xy			map_size;
 	t_player		player;
 }	t_data;
 
-int		ft_cub_init(t_data *data, int argc, char **argv);
-int		ft_cub_loop(t_data *data);
+/* *** game init *** */
+int			ft_cub_init(t_data *data, int argc, char **argv);
 
-void	ft_cub(void *param);
-void	ft_draw(t_data *data, t_ray *ray, t_i_xy *xy);
+/* *** game loop *** */
+int			ft_cub_loop(t_data *data);
 
-void	ft_move_player(void *param);
+/* *** raycasting func *** */
+void		ft_cub(void *param);
 
-int		ft_iswall(t_data *data, double x, double y);
-t_u_int	ft_color(t_u_int r, t_u_int g, t_u_int b, t_u_int a);
+/* *** rendering func *** */
+void		ft_draw(t_data *data, t_ray *ray, t_i_xy *xy);
 
-/* **************************** parcing ***************************** */
+/* *** moving func *** */
+void		ft_move_player(void *param);
+
+/* *** utils funcs *** */
+int			ft_iswall(t_data *data, double x, double y);
+t_u_int		ft_color(t_u_int r, t_u_int g, t_u_int b, t_u_int a);
+
+/* ********************************* parcing ******************************** */
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
 
-typedef struct	s_point
+typedef struct s_point
 {
-	int x;
-	int y;
-} t_point;
+	int	x;
+	int	y;
+}	t_point;
 
 typedef struct s_gc
 {
-	void			*ptr;
-	struct s_gc		*next;
+	void		*ptr;
+	struct s_gc	*next;
 }	t_gc;
 
 typedef struct s_textures
 {
-	char *no;
-	char *so;
-	char *we;
-	char *ea;
-} t_textures;
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+}	t_textures;
 
 typedef struct s_color
 {
-	int r;
-	int g;
-	int b;
-} t_color;
-
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
 
 typedef struct s_config
 {
-	t_textures tex;
-	t_color floor;
-	t_color ceil;
-	char **map;
-	int player_x;
-	int player_y;
-	char player_dir;
-	int y_rows;
-	int x_cols;
-} t_config;
-	
+	t_textures	tex;
+	t_color		floor;
+	t_color		ceil;
+	char		**map;
+	int			player_x;
+	int			player_y;
+	char		player_dir;
+	int			y_rows;
+	int			x_cols;
+}	t_config;
+
 //malloc_free;
-void free_ptr(void *ptr);
-void	free_all(void);
-void	*gc_malloc(size_t size);
-char *gc_strndup(const char *line, int start, int end);
-char	*ft_strdup(char *s1);
+void		free_ptr(void *ptr);
+void		free_all(void);
+void		*gc_malloc(size_t size);
+char		*gc_strndup(const char *line, int start, int end);
+char		*ft_strdup(char *s1);
 
 //parcing
-int main_parce(char **cub_file, t_config *data);
-void	data_init(t_config *conf);
-int parce_line(char **file, int fd,t_config *data);
-int assign_color(char *start,char *line, int i, t_config *data);
-int is_map(char *line);
-int assign_tex(char *start,char *line, int i, t_config *data);
-int negative_map_position(t_config *data);
-int only_spaces(char *s);
-int get_cont_line(char *file);
-char *get_start(char *line, int i);
-char *get_color(char *line, int i);
+int			main_parce(char **cub_file, t_config *data);
+void		data_init(t_config *conf);
+int			parce_line(char **file, int fd, t_config *data);
+int			assign_color(char *start, char *line, int i, t_config *data);
+int			is_map(char *line);
+int			assign_tex(char *start, char *line, int i, t_config *data);
+int			negative_map_position(t_config *data);
+int			only_spaces(char *s);
+int			get_cont_line(char *file);
+char		*get_start(char *line, int i);
+char		*get_color(char *line, int i);
 
 //utils
-int	ft_isdigit(int v);
-int	ft_atoi(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_strcmp(char *s1, char *s2);
-int is_space(char c);
-
+int			ft_isdigit(int v);
+int			ft_atoi(const char *str);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(char *s1, char *s2);
+int			is_space(char c);
 
 //validation
-int validation(t_config *data);
-int is_player(char c);
-int	flood_fill(char **map, t_point p, int height, int width);
-int validation_char_map(t_config * data);
-char **prepar_map(t_config *data);
-int get_long(t_config *data);
-int get_h(char**map);
-int	fill_inside(char **map, t_point p, t_config *data);
-int check_inside(char **map, t_config *data);
-void assing_position(int y, int x, t_config *data);
-void	find_and_replace_player(char **map, t_config *data);
-
+int			validation(t_config *data);
+int			is_player(char c);
+int			flood_fill(char **map, t_point p, int height, int width);
+int			validation_char_map(t_config *data);
+char		**prepar_map(t_config *data);
+int			get_long(t_config *data);
+int			get_h(char**map);
+int			fill_inside(char **map, t_point p, t_config *data);
+int			check_inside(char **map, t_config *data);
+void		assing_position(int y, int x, t_config *data);
+void		find_and_replace_player(char **map, t_config *data);
 
 //grtnext_line
-char	*get_next_line(int fd);
-char	*ft_read_to_left_str(int fd, char *left_str);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *s);
-char	*ft_strjoin(char *left_str, char *buff);
-char	*ft_get_line(char *left_str);
-char	*ft_new_left_str(char *left_str);
-t_config *parcing(int ac, char **av);
+char		*get_next_line(int fd);
+char		*ft_read_to_left_str(int fd, char *left_str);
+char		*ft_strchr(char *s, int c);
+size_t		ft_strlen(char *s);
+char		*ft_strjoin(char *left_str, char *buff);
+char		*ft_get_line(char *left_str);
+char		*ft_new_left_str(char *left_str);
+t_config	*parcing(int ac, char **av);
 
 #endif
